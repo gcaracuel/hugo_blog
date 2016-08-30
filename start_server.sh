c7aedf264d381e7474d2ecbd62ccf7b19ebeafe5
Guillermo Caracuel <gcaracuel@gmail.com>
2016-08-30 23:45:14 +0200

    Starting the start script to generate the site

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source_dir="/vagrant/blogsite"
 
-hugo server --bind="0.0.0.0" --port=1300 --theme="hugo-theme-casper" --source=$source_dir --buildDrafts --watch --logFile="/tmp/blosite.log" >/dev/null 2>&1 < /dev/null &
+hugo server --bind="0.0.0.0" --port=1300 --theme="hugo-theme-casper" --source=$source_dir --destination="$source_dir/public"--buildDrafts --watch --logFile="/tmp/blosite.log" >/dev/null 2>&1 < /dev/null &
