1b36287c20ec6c19922974b116e9073b1a60a907
Guillermo Caracuel <gcaracuel@gmail.com>
2016-08-31 00:09:56 +0200

    Fixing start script to server and build

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source_dir="/vagrant/blogsite"
 
-hugo server --bind="0.0.0.0" --port=1300 --theme="hugo-theme-casper" --source=$source_dir --destination="$source_dir/public"--buildDrafts --watch --logFile="/tmp/blosite.log" >/dev/null 2>&1 < /dev/null &
+hugo server --bind="0.0.0.0" --port=1300 --theme="hugo-theme-casper" --source=$source_dir --buildDrafts --watch --logFile="/tmp/blosite.log" >/dev/null 2>&1 < /dev/null &
+
+# Now build the same files we are serving
+hugo -s $source_dir -d public/
